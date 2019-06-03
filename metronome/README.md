635eaf5e8121a473e8f6b57051358c73c1efe044
Ivan Z <szi8890@users.noreply.github.com>
2019-06-03 16:38:07 -0700

    Update all info

---
+++ metronome/README.md
@@ -0,0 +1,2 @@
+A simple metronome that has a slider adjuster for BPM. Created as an intro
+to Reactjs.
---
+++ metronome/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Metronome from './Metronome';
+import './index.css';
+
+ReactDOM.render(<Metronome />, document.getElementById('root'));
